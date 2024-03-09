64bf35fda015318ccbb3b65f195d2bf80ba5472e
Razzile <comfycallum@gmail.com>
2024-03-09 17:19:14 +0000

    [Qt] Sidebar documentation.

--- src/xenia/ui/qt/widgets/sidebar.h
+++ src/xenia/ui/qt/widgets/sidebar.h
@@ -13,13 +13,36 @@ class XSideBar : public Themeable<QToolBar> {
  public:
   explicit XSideBar();
 
+  /**
+   * Adds a new action (button) to this sidebar.
+   * @param text title to give this action.
+   * @return a pointer to the created button.
+   */
   XSideBarButton* addAction(const QString& text);
+  /**
+   * Adds a new action (button) to this sidebar.
+   * @param glyph a glyph code to assign a glyph icon to the created button.
+   * @param text title to give this action.
+   * @return a pointer to the created button.
+   */
   XSideBarButton* addAction(QChar glyph, const QString& text);
 
+  /**
+   * Add a spacer to this sidebar. A spacer is just an empty QWidget.
+   * @param size size of the spacer.
+   * @return a pointer to the created spacer.
+   */
   QWidget* addSpacing(int size);
 
+  /**
+   * @return the button group containing all buttons in this sidebar.
+   */
   QButtonGroup* buttonGroup() const { return buttons_; }
 
+  /**
+   * Gets the currently selected sidebar button.
+   * @return the index of currently selected button, or -1 if none selected.
+   */
   int currentSelection() const;
 
  protected:
